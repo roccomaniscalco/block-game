d220a4de9ec7d95bc51fe681a1fdfe5daec8b879
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2024-01-09 05:12:12 -0500

    render `GameBoard`

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
-function App() {
-  return <div className="text-3xl text-white">hi</div>;
+import { useState } from "react";
+
+export default function App() {
+  return (
+    <div className="text-3xl text-white">
+      <GameBoard />
+    </div>
+  );
 }
 
-export default App;
+function GameBoard() {
+  const initialBoard = [
+    [".", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  ];
+
+  const [board, setBoard] = useState(initialBoard);
+
+  return (
+    <div className="grid aspect-square grid-cols-9 grid-rows-9 gap-1">
+      {board.map((row) => row.map((cell, j) => <div key={j}>{cell}</div>))}
+    </div>
+  );
+}
