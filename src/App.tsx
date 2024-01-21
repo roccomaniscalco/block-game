ba55bbc7a0e3eac55065a269fd8b6d6977d5c0f0
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2024-01-21 16:10:08 -0500

    create `isLightSquare` to style board squares

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,35 @@ export default function App() {
   );
 }
 
-function GameBoard() {
-  const initialBoard = [
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-  ];
+function isLightSquare(rowI: number, colI: number) {
+  const isInOddRow = Math.floor(rowI / 3) % 2 === 1;
+  const isInOddCol = Math.floor(colI / 3) % 2 === 1;
+  return isInOddRow !== isInOddCol;
+}
 
-  const [board, setBoard] = useState(initialBoard);
+const INITIAL_BOARD = [
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "."],
+];
+
+function GameBoard() {
+  const [board, setBoard] = useState(INITIAL_BOARD);
 
   return (
     <div className="m-auto grid aspect-square max-w-lg grid-cols-9 grid-rows-9 gap-1 p-3">
-      {board.map((row) =>
-        row.map((cell, j) => (
-          <div className="text-center" key={j}>
+      {board.map((row, rowI) =>
+        row.map((cell, colI) => (
+          <div
+            className={isLightSquare(rowI, colI) ? "text-red-500" : "text-blue-500"}
+            key={colI}
+          >
             {cell}
           </div>
         )),
