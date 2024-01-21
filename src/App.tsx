d3d2d34b900f1101c26a3e636eb65af42222cc30
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2024-01-21 18:19:54 -0500

    refactor to better encapsulate components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { DndContext, useDraggable, useDroppable } from "@dnd-kit/core";
 import { CSS } from "@dnd-kit/utilities";
-import { ReactNode, useState } from "react";
+import { useId, useState } from "react";
 import { cn } from "./utils";
 
 export default function App() {
@@ -17,12 +17,6 @@ function Game() {
   ) as boolean[][];
   const [tiles, setTiles] = useState(INITIAL_TILES);
 
-  const piece = (
-    <Piece id="tile">
-      <div className="h-10 w-10 bg-red-400" />
-    </Piece>
-  );
-
   return (
     <DndContext
       onDragEnd={(event) => {
@@ -32,7 +26,7 @@ function Game() {
       }}
     >
       <Board tiles={tiles} />
-      {piece}
+      <Piece />
     </DndContext>
   );
 }
@@ -41,6 +35,42 @@ type BoardProps = {
   tiles: boolean[][];
 };
 function Board(props: BoardProps) {
+  return (
+    <div className="m-auto grid aspect-square max-w-xl grid-cols-9 grid-rows-9 gap-1 p-3">
+      {props.tiles.map((row, y) =>
+        row.map((tile, x) => <Tile x={x} y={y} key={x} isFilled={tile} />),
+      )}
+    </div>
+  );
+}
+
+function Piece() {
+  const id = useId();
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: id,
+  });
+
+  return (
+    <button
+      className="h-10 w-10 bg-red-400"
+      style={{ transform: CSS.Translate.toString(transform) }}
+      ref={setNodeRef}
+      {...listeners}
+      {...attributes}
+    ></button>
+  );
+}
+
+type TileProps = {
+  x: number;
+  y: number;
+  isFilled: boolean;
+};
+export function Tile(props: TileProps) {
+  const { isOver, setNodeRef } = useDroppable({
+    id: `${props.y},${props.x}`,
+  });
+
   const isLightSquare = (y: number, x: number) => {
     const isInOddRow = Math.floor(y / 3) % 2 === 1;
     const isInOddCol = Math.floor(x / 3) % 2 === 1;
@@ -48,59 +78,14 @@ function Board(props: BoardProps) {
   };
 
   return (
-    <div className="m-auto grid aspect-square max-w-xl grid-cols-9 grid-rows-9 gap-1 p-3">
-      {props.tiles.map((row, y) =>
-        row.map((tile, x) => (
-          <Tile id={`${y},${x}`} key={x}>
-            <div
-              className={cn(
-                "h-full w-full rounded-md",
-                isLightSquare(y, x) ? "bg-gray-700" : "bg-gray-800",
-                tile && "bg-red-400",
-              )}
-            />
-          </Tile>
-        )),
+    <div
+      ref={setNodeRef}
+      className={cn(
+        "h-full w-full rounded-md",
+        isLightSquare(props.y, props.x) ? "bg-gray-700" : "bg-gray-800",
+        props.isFilled && "bg-red-400",
+        isOver && "brightness-150",
       )}
-    </div>
-  );
-}
-
-type PieceProps = {
-  id: string;
-  children: ReactNode;
-};
-function Piece(props: PieceProps) {
-  const { attributes, listeners, setNodeRef, transform } = useDraggable({
-    id: props.id,
-  });
-  const style = {
-    // Outputs `translate3d(x, y, 0)`
-    transform: CSS.Translate.toString(transform),
-  };
-
-  return (
-    <button ref={setNodeRef} style={style} {...listeners} {...attributes}>
-      {props.children}
-    </button>
-  );
-}
-
-type TileProps = {
-  id: string;
-  children: ReactNode;
-};
-export function Tile(props: TileProps) {
-  const { isOver, setNodeRef } = useDroppable({
-    id: props.id,
-  });
-  const style = {
-    opacity: isOver ? 1 : 0.8,
-  };
-
-  return (
-    <div ref={setNodeRef} style={style}>
-      {props.children}
-    </div>
+    />
   );
 }
