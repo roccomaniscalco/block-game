2602f283668dae71f8c8c2294c7c566781026602
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2024-01-22 05:03:49 -0500

    use utils to clean up and decrease use of `as`

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,26 @@
 import { DndContext, useDraggable, useDroppable } from "@dnd-kit/core";
 import { CSS } from "@dnd-kit/utilities";
 import { useState } from "react";
-import { cn } from "./utils";
+import { array, cn, getObjectKeys } from "./utils";
 import SHAPES from "./shapes.json";
-
-type ShapeName = keyof typeof SHAPES;
 
 export default function App() {
   return <Game />;
 }
 
 function Game() {
-  const INITIAL_TILES = Array.from({ length: 9 }, () =>
-    Array(9).fill(false),
-  ) as boolean[][];
+  const INITIAL_TILES = array(9, array(9, false));
 
   const getRandomShape = () => {
-    const shapes = Object.keys(SHAPES) as ShapeName[];
+    const shapes = getObjectKeys(SHAPES);
     const randomShapeName = shapes[Math.floor(Math.random() * shapes.length)];
     return SHAPES[randomShapeName];
   };
 
-  const initialShape = [...Array(3).keys()].map(getRandomShape);
+  const initialShapes = array(3).map(getRandomShape);
 
   const [tiles, setTiles] = useState(INITIAL_TILES);
-  const [shapes, setShapes] = useState(initialShape);
+  const [shapes, setShapes] = useState(initialShapes);
 
   return (
     <DndContext
@@ -33,7 +29,7 @@ function Game() {
         const { x, y } = event.over.data.current as { x: number; y: number };
         if (tiles[y][x]) return tiles;
         setTiles(tiles.with(y, tiles[y].with(x, true)));
-        setShapes(shapes.toSpliced(event.active.id.split("-")[1], 1));
+        setShapes(shapes.toSpliced(Number(event.active.id), 1));
       }}
     >
       <main className="flex h-full flex-col gap-10 p-5">
@@ -67,7 +63,7 @@ function ShapePalette(props: ShapePaletteProps) {
     <div className="mx-auto flex w-full max-w-xl items-center justify-evenly gap-5">
       {props.shapes.map((shape, idx) => (
         <div className="flex flex-1 justify-center">
-          <Shape key={idx} id={`shape-${idx}`} shape={shape} />
+          <Shape key={idx} id={idx.toString()} shape={shape} />
         </div>
       ))}
     </div>
@@ -79,10 +75,9 @@ type ShapeProps = {
   shape: number[][];
 };
 function Shape(props: ShapeProps) {
-  const { attributes, listeners, setNodeRef, transform } =
-    useDraggable({
-      id: props.id,
-    });
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: props.id,
+  });
 
   return (
     <button
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,31 @@
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * Merges and conditionally applies Tailwind classes
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+/**
+ * Returns a correctly typed array of keys of the object
+ */
+export const getObjectKeys = Object.keys as <T extends object>(
+  obj: T,
+) => Array<keyof T>;
+
+/**
+ * Returns a correctly typed array of values of the object
+ */
+export const getObjectValues = Object.values as <T extends object>(
+  obj: T,
+) => Array<(typeof obj)[keyof typeof obj]>;
+
+export function array<T>(length: number): T[];
+export function array<T>(length: number, value: T): T[];
+
+export function array<T>(length: number, value?: T) {
+  if (!value) return Array.from({ length });
+  return Array.from({ length }, () => value);
 }
